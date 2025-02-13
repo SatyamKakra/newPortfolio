efe7715604488d42abfd7846431976894598aa1a
satyam kakra <satyamkakra4u@gmail.com>
2025-02-13 12:39:07 +0530

    my commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {BrowserRouter as Router} from 'react-router-dom'
+// import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    {/* <Router> */}
     <App />
 
-    </Router>
+    {/* </Router> */}
   </React.StrictMode>
 );
 
