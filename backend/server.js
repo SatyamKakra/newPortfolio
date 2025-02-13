63971ca716ed0877d82497dc9e21e670737399fc
satyam kakra <satyamkakra4u@gmail.com>
2025-02-13 17:30:16 +0530

    save

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({ origin: "https://satyamkakra.github.io" }));
 app.use(bodyParser.json());
 
+
 // Nodemailer Transporter
 const transporter = nodemailer.createTransport({
   service: "gmail",
