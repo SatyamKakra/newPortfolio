1ce97feab0fefedecf4d4962e7ab80e9de8d9c51
satyam kakra <satyamkakra4u@gmail.com>
2025-02-13 17:13:18 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "https://satyamkakra.github.io" }));
 app.use(bodyParser.json());
 
 // Nodemailer Transporter
